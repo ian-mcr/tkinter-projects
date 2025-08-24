098ca303ec7fcf85340b447efaf5494dcd2850cc
ian-mcr <115840092+ian-mcr@users.noreply.github.com>
2025-08-24 17:00:31 +0100

    Add files via upload

---
+++ math table.py
@@ -0,0 +1,39 @@
+import tkinter
+import tkinter.ttk
+screen=tkinter.Tk()
+screen.geometry("400x500")
+screen .title("")
+
+label1=tkinter.Label(screen,text="Mathematicl Table")
+label2=tkinter.Label(screen,text="Number and Range")
+label3=tkinter.Label(screen,text="")
+
+button1=tkinter.Button(screen,text="Generate")
+
+combobox1=tkinter.ttk.Combobox(screen)
+numbers=[]
+for i in range(1,101):
+    numbers.append(i)
+combobox1["values"]=numbers
+
+choose=tkinter.IntVar()
+
+
+r1=tkinter.Radiobutton(screen,text="10",variable=choose,value=10)
+r2=tkinter.Radiobutton(screen,text="20",variable=choose,value=20)
+r3=tkinter.Radiobutton(screen,text="30",variable=choose,value=30)
+
+label1.grid(row=1,column=2)
+label2.grid(row=2,column=1)
+label3.grid(row=6,column=2)
+
+combobox1.grid(row=2,column=2)
+
+r1.grid(row=2,column=3)
+r2.grid(row=3,column=3)
+r3.grid(row=4,column=3)
+
+button1.grid(row=5,column=2)
+
+
+screen.mainloop()
